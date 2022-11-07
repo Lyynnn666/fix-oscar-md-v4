f8358941b5ec5f61a4bd9198cf969ef20194add1
Lyynnn666 <113133380+Lyynnn666@users.noreply.github.com>
2022-11-07 09:15:15 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -40,25 +40,25 @@ let handler = async (m, {conn}) => {
 const vcard = `BEGIN:VCARD
 VERSION:3.0
 N:;;;
-FN: ᯤ 𝙅𝙖𝙧𝙤𝙩 𝙊𝙛𝙘
+FN: ᯤ L y n n 亥
 item.ORG: ᴅᴇᴠᴇʟᴏᴩᴇʀ ʙᴏᴛ
-item1.TEL;waid=6285850539404:6285850539404@s.whatsapp.net
+item1.TEL;waid=6283890585717:6283890585717@s.whatsapp.net
 item1.X-ABLabel: ᴅᴇᴠᴇʟᴏᴩᴇʀ ʙᴏᴛ
-item2.EMAIL;type=INTERNET: Jarott@gmail.com
+item2.EMAIL;type=INTERNET: tsyaachann@gmail.com
 item2.X-ABLabel:📫 Gmail
-item3.ADR:;;📍 Jawa Timur - Indonesian;;;;
+item3.ADR:;;📍 Tokyo - Japan;;;;
 item3.X-ABADR:ac
 item3.X-ABLabel:📮 Location Owner
-item4.URL:https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ
+item4.URL:https://youtube.com/channel/UCauQ8ous-UOTqj27lEtKUhQ
 item4.X-ABLabel:Youtube
-item5.URL:https://github.com/Alpiii22
+item5.URL:_
 item5.X-ABLabel:Github
-item6.URL:https://instagram.com/jarotr_
-item6.X-ABLabel:Instagram Jarot
-item7.URL:𝚈𝚘𝚞𝚝𝚞𝚋𝚎: 𝙹𝚊𝚛𝚘𝚝 𝙾𝚏𝚏𝚌
+item6.URL:_
+item6.X-ABLabel:Gakda ig
+item7.URL:𝚈𝚘𝚞𝚝𝚞𝚋𝚎: Lynn
 item7.X-ABLabel: 𝚂𝚞𝚜𝚌𝚛𝚒𝚋𝚎 𝙲𝚑𝚊𝚗𝚎𝚕 𝚓𝚊𝚛𝚘𝚝
-item1.TEL;waid=6285943036511:6285850539404@s.whatsapp.net
-item1.X-ABLabel: Creator Bot
+item1.TEL;waid=77003156466:77003156466@s.whatsapp.net
+item1.X-ABLabel: My
 END:VCARD`
 const sentMsg  = await conn.sendMessage(
     m.chat,
@@ -69,7 +69,7 @@ const sentMsg  = await conn.sendMessage(
         }
     }
 )
-let jarot = 'https://telegra.ph/file/1b4913c4196e640732efa.jpg'
+let jarot = 'https://telegra.ph/file/3aa261c3f088e389f2e1c.jpg'
 await conn.send3ButtonImg(m.chat, jarot, `${ucapan()}\n\n𝙷𝚊𝚕𝚘 𝙺𝚊𝚔, @${m.sender.split`@`[0]} 👋\n𝙸𝚗𝚒 𝙽𝚘𝚖𝚎𝚛 𝙾𝚠𝚗𝚎𝚛 𝙱𝚘𝚝, 𝙹𝚊𝚗𝚐𝚊𝚗 𝙳𝚒 𝚂𝚙𝚊𝚖 𝚈𝚊\n𝙺𝚊𝚕𝚊𝚞 𝙼𝚊𝚞 𝙳𝚒𝚜𝚊𝚟𝚎 𝚂𝚢𝚊𝚛𝚊𝚝 𝙽𝚢𝚊 𝙷𝚊𝚛𝚞𝚜 𝙿𝚊𝚔𝚊𝚒 𝙿𝚛𝚘𝚏𝚒𝚕 𝚜𝚎𝚗𝚍𝚒𝚛𝚒\n𝙺𝚊𝚕𝚊𝚞 𝙶𝚊𝚔 𝙰𝚍𝚊 𝙿𝚛𝚘𝚏𝚒𝚕 𝙶𝚊𝚔 𝙱𝚊𝚔𝚊𝚕𝚊𝚗 𝙳𝚒 𝚁𝚎𝚜𝚙𝚘𝚗\n𝙹𝚒𝚔𝚊 𝙿𝚎𝚗𝚝𝚒𝚗𝚐 𝙻𝚊𝚗𝚐𝚜𝚞𝚗𝚐 𝚆𝚊 𝙾𝚠𝚗𝚎𝚛\n\n⫹⫺ Date : *${week} ${date}*\n⫹⫺ Time : *${wib}*`, wm, 'Source', '.sc', 'Menu', '.menu', 'Donasi', '.donasi', sentMsg)}
 handler.help = ['owner']
 handler.tags = ['info']
